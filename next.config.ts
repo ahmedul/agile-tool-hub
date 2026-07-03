0855610f05fa08f87095f7483053bb39f3c5f01c
Ahmedul Kabir <aditz_55@hotmail.com>
2026-07-03 21:55:31 +0200

    seo: harden host/protocol canonical redirects

--- next.config.ts
+++ next.config.ts
@@ -14,6 +14,38 @@ const nextConfig: NextConfig = {
           type: "host",
           value: "www.agiletoolhub.com",
         },
+        {
+          type: "header",
+          key: "x-forwarded-proto",
+          value: "http",
+        },
+      ],
+      destination: "https://agiletoolhub.com/:path*",
+      permanent: true,
+    },
+    {
+      source: "/:path*",
+      has: [
+        {
+          type: "host",
+          value: "agiletoolhub.com",
+        },
+        {
+          type: "header",
+          key: "x-forwarded-proto",
+          value: "http",
+        },
+      ],
+      destination: "https://agiletoolhub.com/:path*",
+      permanent: true,
+    },
+    {
+      source: "/:path*",
+      has: [
+        {
+          type: "host",
+          value: "www.agiletoolhub.com",
+        },
       ],
       destination: "https://agiletoolhub.com/:path*",
       permanent: true,
